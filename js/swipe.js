b75fec826b5e3c50cd28184db1a8f07219213171
kalkulator413 <kalkin413@gmail.com>
2025-05-17 11:57:33 -0700

    hacked together card swiping

--- js/swipe.js
+++ js/swipe.js
@@ -1,5 +1,3 @@
-// nathans umerged code
-
 document.addEventListener('DOMContentLoaded', async () => {
     let data = JSON.parse(localStorage.getItem('restaurantData'));
 
@@ -12,45 +10,68 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     renderRestaurants(data);
 
+    setupButtons();
+
 });
+
+let current_idx = 0;
 
 function renderRestaurants(data) {
     const container = document.getElementById('card-container');
     container.innerHTML ='';
 
+    let idx = 0;
     data.forEach(r => {
         const div = document.createElement('div');
         div.classList.add('restaurant-card');
          div.innerHTML = `
-      <h3>${r.name}</h3>
-      <p>Cuisine: ${r.cuisine}</p>
-      <p>Price: ${r.price}</p>
-      <p>Distance: ${r.distance} mi</p>
-      <p>Rating: ${r.rating}</p>
-      <button onclick="saveToDeck(${r.id})">Save to Deck</button>
-    `;
-    container.appendChild(div);
+            <h2>${r["name"]}</h2>
+            <img
+                src="assets/restaurant.jpg"
+                alt="restaurant"
+                class="card-img"
+            />
+            <div class="details">
+                <span class="rating">⭐ ${r["rating"]}</span>
+                <span class="distance">📍 ${r["distance"]} mi</span>
+            </div>
+            <div class="tags">
+                <span class="tag">${r["cuisine"]}</span>
+                <span class="tag">cuisine</span>
+                <span class="tag">+2</span>
+            </div>
+            </div>
+        `;
+        div.id = `restaurant_${idx}`;
+        div.style.display = "none";
+        idx += 1;
+        container.appendChild(div);
+    });
 
-    /*
-<div class="restaurant-card" data-id="1" data-name="Pizza Place">
-          <h2>Restaurant Title</h2>
-          <img
-            src="assets/restaurant.jpg"
-            alt="restaurant"
-            class="card-img"
-          />
-          <div class="details">
-            <span class="rating">⭐ 4.3</span>
-            <span class="distance">📍 10 mi</span>
-          </div>
-          <div class="tags">
-            <span class="tag">cuisine</span>
-            <span class="tag">cuisine</span>
-            <span class="tag">+2</span>
-          </div>
-        </div>
-    */
-  });
+    let first = document.querySelector("#restaurant_0");
+    first.style.display = 'block';
+}
+
+function setupButtons() {
+    let rej = document.querySelector('button[title="Reject"]');
+    rej.addEventListener("click", () => {
+        let current = document.querySelector(`#restaurant_${current_idx}`);
+        current.style.display = 'none';
+
+        current_idx += 1;
+        let newEl = document.querySelector(`#restaurant_${current_idx}`);
+        newEl.style.display = 'block';
+    });
+
+    let accept = document.querySelector('button[title="Accept"]');
+    accept.addEventListener("click", () => {
+        let current = document.querySelector(`#restaurant_${current_idx}`);
+        current.style.display = 'none';
+
+        current_idx += 1;
+        let newEl = document.querySelector(`#restaurant_${current_idx}`);
+        newEl.style.display = 'block';
+    });
 }
 
 // -------------------
