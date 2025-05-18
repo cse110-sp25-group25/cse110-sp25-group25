84aef46bf64b1266418a84932527aff0a34e91d6
kalkulator413 <kalkin413@gmail.com>
2025-05-17 19:17:03 -0700

    remove weird buffering

--- js/swipe.js
+++ js/swipe.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 
     renderRestaurant(data);
-    setupButtons(data);
+    setupButtons();
 
 });
 
@@ -18,108 +18,70 @@ function renderRestaurant(data) {
     const container = document.getElementById('card-container');
     container.innerHTML ='';
 
-    let r = data[0];
-    const div = document.createElement('div');
-    div.classList.add('restaurant-card');
-    div.innerHTML = `
-        <h2>${r["name"]}</h2>
-        <img
-            src="assets/restaurant.jpg"
-            alt="restaurant"
-            class="card-img"
-        />
-        <div class="details">
-            <span class="rating">⭐ ${r["rating"]}</span>
-            <span class="distance">📍 ${r["distance"]} mi</span>
-        </div>
-        <div class="tags">
-            <span class="tag">${r["cuisine"]}</span>
-            <span class="tag">cuisine</span>
-            <span class="tag">+2</span>
-        </div>
-        </div>
-    `;
-    div.id = r["id"];
-    div.style.display = "block";
-    container.appendChild(div);
-}
-
-function setupButtons(data) {
-    let rej = document.querySelector('button[title="Reject"]');
-    rej.addEventListener("click", () => {
-        let current = document.querySelector(`.restaurant-card`);
-        current.classList.add('swipe-right');
-
-        setTimeout(() => {
-            current.style.display = 'none';
-            current.classList.remove('swipe-right');
-            newId = +current.id;
-            let r = data[newId];
-
-            current.innerHTML = `
-                <h2>${r["name"]}</h2>
-                <img
-                    src="assets/restaurant.jpg"
-                    alt="restaurant"
-                    class="card-img"
-                />
-                <div class="details">
-                    <span class="rating">⭐ ${r["rating"]}</span>
-                    <span class="distance">📍 ${r["distance"]} mi</span>
-                </div>
-                <div class="tags">
-                    <span class="tag">${r["cuisine"]}</span>
-                    <span class="tag">cuisine</span>
-                    <span class="tag">+2</span>
-                </div>
-                </div>
-            `;
-            current.id = r["id"];
-
-            setTimeout(() => {
-                current.style.display = 'block';
-            }, 50);
-            
-        }, 300);
+    data.forEach(r => {
+        const div = document.createElement('div');
+        div.classList.add('restaurant-card');
+        div.innerHTML = `
+            <h2>${r["name"]}</h2>
+            <img
+                src="assets/restaurant.jpg"
+                alt="restaurant"
+                class="card-img"
+            />
+            <div class="details">
+                <span class="rating">⭐ ${r["rating"]}</span>
+                <span class="distance">📍 ${r["distance"]} mi</span>
+            </div>
+            <div class="tags">
+                <span class="tag">${r["cuisine"]}</span>
+                <span class="tag">cuisine</span>
+                <span class="tag">+2</span>
+            </div>
+            </div>
+        `;
+        div.id = r["id"];
+        div.style.display = "none";
+        container.appendChild(div);
     });
 
-    let accept = document.querySelector('button[title="Accept"]');
-    accept.addEventListener("click", () => {
-        let current = document.querySelector(`.restaurant-card`);
+    let first = document.getElementById('1');
+    first.classList.add("active-card");
+    first.style.display = 'block';
+}
+
+function setupButtons() {
+    let rej = document.querySelector('button[title="Reject"]');
+    rej.addEventListener("click", () => {
+        let current = document.querySelector('.active-card');
         current.classList.add('swipe-left');
 
         setTimeout(() => {
             current.style.display = 'none';
             current.classList.remove('swipe-left');
-            newId = +current.id;
-            let r = data[newId];
-
-            current.innerHTML = `
-                <h2>${r["name"]}</h2>
-                <img
-                    src="assets/restaurant.jpg"
-                    alt="restaurant"
-                    class="card-img"
-                />
-                <div class="details">
-                    <span class="rating">⭐ ${r["rating"]}</span>
-                    <span class="distance">📍 ${r["distance"]} mi</span>
-                </div>
-                <div class="tags">
-                    <span class="tag">${r["cuisine"]}</span>
-                    <span class="tag">cuisine</span>
-                    <span class="tag">+2</span>
-                </div>
-                </div>
-            `;
-            current.id = r["id"];
-
-            setTimeout(() => {
-                current.style.display = 'block';
-            }, 50);
+            current.classList.remove('active-card')
+            newId = Number(current.id) + 1;
             
+            let newChild = document.getElementById(newId)
+            newChild.style.display = 'block';
+            newChild.classList.add('active-card');
         }, 300);
+    });
 
+    let accept = document.querySelector('button[title="Accept"]');
+    accept.addEventListener("click", () => {
+        let current = document.querySelector('.active-card');
+        current.classList.add('swipe-right');
+
+        setTimeout(() => {
+            current.style.display = 'none';
+            current.classList.remove('swipe-right');
+            current.classList.remove('active-card')
+            newId = Number(current.id) + 1;
+            
+            let newChild = document.getElementById(newId)
+            newChild.style.display = 'block';
+            newChild.classList.add('active-card');
+        }, 300);
     });
 }
 
