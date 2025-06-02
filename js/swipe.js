e1ef32fd29560d2d15830783074f4d8e6fcd1fba
ebluong005 <130106644+ebluong005@users.noreply.github.com>
2025-06-01 17:19:56 -0700

    merge conflicts

--- js/swipe.js
+++ js/swipe.js
@@ -1,3 +1,5 @@
+// import {applyFilters, removeViewed} from './swipe.utils.js'
+
 document.addEventListener('DOMContentLoaded', async () => {
   let data = JSON.parse(localStorage.getItem('restaurantData'));
 
@@ -26,18 +28,34 @@ document.addEventListener('DOMContentLoaded', async () => {
   setupButtons();
 });
 
-function applyFilters(data, filters) {
-  return data.filter(r => {
-      if (filters.cuisine && r.cuisine !== filters.cuisine) return false;
-      if (filters.price && r.price.length > filters.price.length) return false;
-      // Parse distance and rating
-      if (filters.distance && r.distance > parseFloat(filters.distance)) return false;
-      if (filters.rating && r.rating < parseFloat(filters.rating)) return false;
 
-      return true;
+/**
+ * A function to remove the subset of data that doesn't satisfy the desired filters
+ * @param {Array} data original list of data
+ * @param {Array} filters list of filters to apply
+ * @returns the subset of data that satisfies the desired filters
+ */
+function applyFilters(data, filters) {
+    return data.filter(r => {
+        if (filters.cuisine && filters.cuisine.length>0 && !filters.cuisine.includes(r.cuisine)){
+            return false;
+        } 
+        if (filters.price && r.price.length > filters.price.length) return false;
+        // Parse distance and rating
+        if (filters.distance && r.distance > parseFloat(filters.distance)) return false;
+        if (filters.rating && r.rating < parseFloat(filters.rating)) return false;
+
+        return true;
   });
 }
 
+/**
+ * A function that takes in a list of data, a list of viewed elements, and returns the subset of the original list
+ * that is not in the second.
+ * @param data original list
+ * @param viewed items to remove
+ * @returns the set difference data \ viewed
+ */
 function removeViewed(data, viewed) {
   return data.filter(r => {
     return !viewed.includes(+r.id);
@@ -300,3 +318,4 @@ function handleViewedCard(id) {
     localStorage.setItem('viewed', JSON.stringify(viewed));
   }
 }
+
