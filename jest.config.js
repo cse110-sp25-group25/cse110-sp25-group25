1df7e488ccb34cab2a1b40c7ef3785845f91da3a
wangharold001 <wangharold001@gmail.com>
2025-06-01 19:01:30 -0700

    added jest coverage by QM

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,18 @@ export default {
   collectCoverage: true,
   collectCoverageFrom: ['js/**/*.utils.js'],
   coverageDirectory: 'docs/coverage',
+  reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: 'docs/coverage', outputName: 'junit.xml' }]
+  ],
   coverageReporters: [
     'json-summary',  // ← tells Jest to write coverage/coverage-summary.json
     'json', 
     'lcov',
-    'text'
+    'text',
+    'cobertura'
   ],
+
   transform: {},    // suppress babel warning
   verbose: true,
   testMatch: ['**/__tests__/**/*.test.js'],
